1ec08df827b07bbbbdac5f272c743fcd7592e043
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-01-24 14:33:03 +0500

    add TestSP console app

---
+++ TestSPConsole/TestSPConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace TestSPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string url = "http://sharepoint:2013/";
+
+            SPSite siteCollection = new SPSite(url);
+            SPWeb site = siteCollection.RootWeb;
+
+            foreach(SPList list in site.Lists)
+            {
+                Console.WriteLine(list.Title);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
