89167b1c5cce89b857eb1585db36897b82187c39
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-01-25 16:03:14 +0500

    get FarmID and isAdmin properties

--- Tests/SPClasses/SPClasses/Program.cs
+++ Tests/SPClasses/SPClasses/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
 
 namespace SPClasses
 {
@@ -10,6 +12,15 @@ namespace SPClasses
     {
         static void Main(string[] args)
         {
+            //Get the unique identifier for the local farm.
+            Guid farmID = SPFarm.Local.Id;
+            //Determine whether the current user is a farm administrator.
+            bool isAdmin = SPFarm.Local.CurrentUserIsAdministrator();
+            Console.WriteLine("FarmID {0}\nisAdmin {1}", farmID, isAdmin);
+
+
+            Console.ReadKey();
+
         }
     }
 }
