6492a1da185fd27f380987a4b5e9251c0c32c81b
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-04 16:06:11 +0500

    add preconfigured WebPart

---
+++ Module5/ExpensesWebPart/ExpensesWebPart/ExpensesInformationWebPart/ExpensesInformationWebPart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace ExpensesWebPart.ExpensesInformationWebPart
+{
+    [ToolboxItemAttribute(false)]
+    public class ExpensesInformationWebPart : WebPart
+    {
+        protected override void CreateChildControls()
+        {
+        }
+
+        protected override void RenderContents(HtmlTextWriter writer)
+        {
+            SPWeb web = SPContext.Current.Web;
+            string keyName = "ContosoDepartmentalExpenseTotal";
+            if (web.Properties[keyName] != null)
+            {
+                string expenseTotal = web.Properties[keyName];
+                writer.Write("<p>Estimated departmental expense total is: " + expenseTotal + "</p>");
+            }
+
+        }
+    }
+}
---
+++ Module5/ExpensesWebPart/ExpensesWebPart/Features/ExpensesInformationWebPartDeployment/ExpensesInformationWebPartDeployment.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="9a94393e-6e59-4bf5-8a95-b0d855d9705c" featureId="9a94393e-6e59-4bf5-8a95-b0d855d9705c" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="ExpensesInformationWebPartDeployment" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="7dcc9cac-038c-4823-9523-4875fba62d9a" />
+  </projectItems>
+</feature>
