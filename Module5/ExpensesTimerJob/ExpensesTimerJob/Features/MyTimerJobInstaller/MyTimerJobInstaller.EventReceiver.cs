17afdb01f0c458b4cdf5376a62f991c23881508d
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-04 18:15:46 +0500

    add a Feature

---
+++ Module5/ExpensesTimerJob/ExpensesTimerJob/Features/MyTimerJobInstaller/MyTimerJobInstaller.EventReceiver.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace ExpensesTimerJob.Features.MyTimerJobInstaller
+{
+    /// <summary>
+    /// Этот класс обрабатывает события, возникающие в ходе активации, деактивации, установки, удаления и обновления компонентов.
+    /// </summary>
+    /// <remarks>
+    /// GUID, присоединенный к этому классу, может использоваться при создании пакета и не должен изменяться.
+    /// </remarks>
+
+    [Guid("423c4e76-6c3b-4d08-b67a-e4d945ca0c0d")]
+    public class MyTimerJobInstallerEventReceiver : SPFeatureReceiver
+    {
+        const string timerJobName = "MyTimerJob";
+        // Раскомментируйте ниже метод для обработки события, возникающего после активации компонента.
+
+        //public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Раскомментируйте ниже метод для обработки события, возникающего перед деактивацией компонента.
+
+        //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Раскомментируйте ниже метод для обработки события, возникающего после установки компонента.
+
+        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Раскомментируйте ниже метод для обработки события, возникающего перед удалением компонента.
+
+        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+        // Раскомментируйте ниже метод для обработки события, возникающего при обновлении компонента.
+
+        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        //{
+        //}
+    }
+}
