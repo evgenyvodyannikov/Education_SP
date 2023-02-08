9daea08ac4a8ba9b43c0fc077b0bb899c5d05f9a
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-08 12:45:03 +0500

    create an event receiver class

---
+++ Module12/VacationRequestsEventReceiver/VacationRequestsEventReceiver/VacationRequestEventReceiver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace VacationRequestsEventReceiver
+{
+    public class VacationRequestEventReceiver : SPItemEventReceiver
+    {
+        public override void ItemAdding(SPItemEventProperties properties)
+        {
+            base.ItemAdding(properties);
+        }
+
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+        }
+    }
+}
