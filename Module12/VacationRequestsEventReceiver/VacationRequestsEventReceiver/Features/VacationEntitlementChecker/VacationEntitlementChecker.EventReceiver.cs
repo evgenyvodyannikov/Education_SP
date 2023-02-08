e565c6227e0366c99625d896d111cf61a22310d3
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-08 18:34:18 +0500

    add FeatureActivated and FeatureDeactivating methods

--- Module12/VacationRequestsEventReceiver/VacationRequestsEventReceiver/Features/VacationEntitlementChecker/VacationEntitlementChecker.EventReceiver.cs
+++ Module12/VacationRequestsEventReceiver/VacationRequestsEventReceiver/Features/VacationEntitlementChecker/VacationEntitlementChecker.EventReceiver.cs
@@ -16,18 +16,152 @@ namespace VacationRequestsEventReceiver.Features.VacationEntitlementChecker
     public class VacationEntitlementCheckerEventReceiver : SPFeatureReceiver
     {
         // Раскомментируйте ниже метод для обработки события, возникающего после активации компонента.
+    
+        const string itemAddingName = "Vacation Request ItemAdding";
+        const string itemUpdatedName = "Vacation Request ItemUpdated";
 
         public override void FeatureActivated(SPFeatureReceiverProperties properties)
         {
-
+            AddItemAddingReceiver(properties);
+            AddItemUpdatedReceiver(properties);
         }
-
-
-        // Раскомментируйте ниже метод для обработки события, возникающего перед деактивацией компонента.
 
         public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
         {
+            RemoveItemAddingReceiver(properties);
+            RemoveItemUpdatedReceiver(properties);
+        }
 
+        private static void AddItemAddingReceiver(SPFeatureReceiverProperties properties)
+        {
+            using (var site = properties.Feature.Parent as SPSite)
+            {
+                using (var web = site.RootWeb)
+                {
+                    // Get the Vacation Request content type.
+                    var vacationRequestCT = web.ContentTypes["Vacation Request"];
+                    if (vacationRequestCT != null)
+                    {
+                        // Check for existing event receivers.
+                        Guid existingEventReceiverId = Guid.Empty;
+                        foreach (SPEventReceiverDefinition eventReceiver in vacationRequestCT.EventReceivers)
+                        {
+                            if (String.Equals(eventReceiver.Name, itemAddingName))
+                            {
+                                existingEventReceiverId = eventReceiver.Id;
+                            }
+                        }
+                        if (!existingEventReceiverId.Equals(Guid.Empty))
+                        {
+                            vacationRequestCT.EventReceivers[existingEventReceiverId].Delete();
+                            vacationRequestCT.Update(true);
+                        }
+
+                        SPEventReceiverDefinition erd = vacationRequestCT.EventReceivers.Add();
+                        erd.Assembly = "VacationRequestsEventReceiver, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b8b5cfca861bb4cc";
+                        erd.Class = "VacationRequestsEventReceiver.VacationRequestEventReceiver";
+                        erd.Type = SPEventReceiverType.ItemAdding;
+                        erd.Name = itemAddingName;
+                        erd.Synchronization = SPEventReceiverSynchronization.Synchronous;
+                        erd.SequenceNumber = 10050;
+                        erd.Update();
+                        vacationRequestCT.Update(true);
+                    }
+                }
+            }
+        }
+
+        private static void AddItemUpdatedReceiver(SPFeatureReceiverProperties properties)
+        {
+            using (var site = properties.Feature.Parent as SPSite)
+            {
+                using (var web = site.RootWeb)
+                {
+                    // Get the Vacation Request content type.
+                    var vacationRequestCT = web.ContentTypes["Vacation Request"];
+                    if (vacationRequestCT != null)
+                    {
+                        // Check for existing event receivers.
+                        Guid existingEventReceiverId = Guid.Empty;
+                        foreach (SPEventReceiverDefinition eventReceiver in vacationRequestCT.EventReceivers)
+                        {
+                            if (String.Equals(eventReceiver.Name, itemUpdatedName))
+                            {
+                                existingEventReceiverId = eventReceiver.Id;
+                            }
+                        }
+                        if (!existingEventReceiverId.Equals(Guid.Empty))
+                        {
+                            vacationRequestCT.EventReceivers[existingEventReceiverId].Delete();
+                            vacationRequestCT.Update(true);
+                        }
+
+                        SPEventReceiverDefinition erd = vacationRequestCT.EventReceivers.Add();
+                        erd.Assembly = "VacationRequestsEventReceiver, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b8b5cfca861bb4cc";
+                        erd.Class = "VacationRequestsEventReceiver.VacationRequestEventReceiver";
+                        erd.Type = SPEventReceiverType.ItemUpdated;
+                        erd.Name = itemUpdatedName;
+                        erd.Synchronization = SPEventReceiverSynchronization.Synchronous;
+                        erd.SequenceNumber = 10050;
+                        erd.Update();
+                        vacationRequestCT.Update(true);
+                    }
+                }
+            }
+        }
+
+        private static void RemoveItemAddingReceiver(SPFeatureReceiverProperties properties)
+        {
+            using (var site = properties.Feature.Parent as SPSite)
+            {
+                using (var web = site.RootWeb)
+                {
+                    var vacationRequestCT = web.ContentTypes["Vacation Request"];
+                    if (vacationRequestCT != null)
+                    {
+                        Guid existingEventReceiverId = Guid.Empty;
+                        foreach (SPEventReceiverDefinition eventReceiver in vacationRequestCT.EventReceivers)
+                        {
+                            if (String.Equals(eventReceiver.Name, itemAddingName))
+                            {
+                                existingEventReceiverId = eventReceiver.Id;
+                            }
+                        }
+                        if (!existingEventReceiverId.Equals(Guid.Empty))
+                        {
+                            vacationRequestCT.EventReceivers[existingEventReceiverId].Delete();
+                            vacationRequestCT.Update(true);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void RemoveItemUpdatedReceiver(SPFeatureReceiverProperties properties)
+        {
+            using (var site = properties.Feature.Parent as SPSite)
+            {
+                using (var web = site.RootWeb)
+                {
+                    var vacationRequestCT = web.ContentTypes["Vacation Request"];
+                    if (vacationRequestCT != null)
+                    {
+                        Guid existingEventReceiverId = Guid.Empty;
+                        foreach (SPEventReceiverDefinition eventReceiver in vacationRequestCT.EventReceivers)
+                        {
+                            if (String.Equals(eventReceiver.Name, itemUpdatedName))
+                            {
+                                existingEventReceiverId = eventReceiver.Id;
+                            }
+                        }
+                        if (!existingEventReceiverId.Equals(Guid.Empty))
+                        {
+                            vacationRequestCT.EventReceivers[existingEventReceiverId].Delete();
+                            vacationRequestCT.Update(true);
+                        }
+                    }
+                }
+            }
         }
 
 
