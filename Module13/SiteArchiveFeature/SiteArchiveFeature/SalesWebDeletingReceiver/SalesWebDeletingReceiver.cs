438d065a73acaf1cf30dbe6a24f14b5f4d9d5e3d
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-02-09 16:19:43 +0500

    create the Web Deleting Event Receiver

---
+++ Module13/SiteArchiveFeature/SiteArchiveFeature/SalesWebDeletingReceiver/SalesWebDeletingReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace SiteArchiveFeature.SalesWebDeletingReceiver
+{
+    /// <summary>
+    /// Веб-события
+    /// </summary>
+    public class SalesWebDeletingReceiver : SPWebEventReceiver
+    {
+        /// <summary>
+        /// Удаляется сайт.
+        /// </summary>
+		public override void WebDeleting(SPWebEventProperties properties)
+        {
+            using (var site = new SPSite(properties.SiteId))
+            {
+                string backupLocation = @"F:\Module13\backups\Backup.backup";
+                site.WebApplication.Sites.Backup(site.Url, backupLocation, true);
+            }
+        }
+    }
+}
